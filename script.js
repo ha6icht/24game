44309078811da54f07c10a973c04e9ba6959654f
rok7803 <55838887+rok7803@users.noreply.github.com>
2024-07-17 22:04:38 +0000

    Add more logic
    Add function getNumberTerm()

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
+const BUTTON_GET_TWENTY_FOUR = document.getElementById('get-twenty-four');
 const GAME_ID = document.getElementById('game');
-const GAME_NUMBER_1 = 1;
-const GAME_NUMBER_2 = 7;
-const GAME_NUMBER_3 = 8;
-const GAME_NUMBER_4 = 9;
-const NUMBER_STRING_1 = GAME_NUMBER_1.toString();
-const NUMBER_STRING_2 = GAME_NUMBER_2.toString();
-const NUMBER_STRING_3 = GAME_NUMBER_3.toString();
-const NUMBER_STRING_4 = GAME_NUMBER_4.toString();
 
 function solve24(numStr){
   const numArr = numStr.slice('');
@@ -30,9 +23,53 @@ function solve24(numStr){
   return termStr;
   //return true;
 }
-const STRING_NUMBERS = NUMBER_STRING_1+NUMBER_STRING_2+NUMBER_STRING_3+NUMBER_STRING_4;
-GAME_ID.innerHTML = solve24(STRING_NUMBERS);
 
-console.log(solve24(STRING_NUMBERS));
+function getNumberTerm(){
+  
+  const GAME_1 = document.getElementById('game-1');
+  const GAME_2 = document.getElementById('game-2');
+  const GAME_3 = document.getElementById('game-3');
+  const GAME_4 = document.getElementById('game-4');
+  
+  const GAME_1_NUMBER_STRING = GAME_1.innerHTML;
+  const GAME_2_NUMBER_STRING = GAME_2.innerHTML;
+  const GAME_3_NUMBER_STRING = GAME_3.innerHTML;
+  const GAME_4_NUMBER_STRING = GAME_4.innerHTML;
+  //const GAME_1_NUMBER = 1;
+  //const GAME_2_NUMBER = 7;
+  //const GAME_3_NUMBER = 8;
+  //const GAME_4_NUMBER = 9;
+
+  const GAME_1_NUMBER = parseInt(GAME_1_NUMBER_STRING);
+  const GAME_2_NUMBER = parseInt(GAME_2_NUMBER_STRING);
+  const GAME_3_NUMBER = parseInt(GAME_3_NUMBER_STRING);
+  const GAME_4_NUMBER = parseInt(GAME_4_NUMBER_STRING);
+
+  const REGEX_ONE_DIGIT = /[0-9]$/g;
+
+  const GAME_1_BOOL = GAME_1_NUMBER_STRING.match(REGEX_ONE_DIGIT)? true : false;
+  const GAME_2_BOOL = GAME_2_NUMBER_STRING.match(REGEX_ONE_DIGIT)? true:false;
+  const GAME_3_BOOL = GAME_3_NUMBER_STRING.match(REGEX_ONE_DIGIT)? true:false;
+  const GAME_4_BOOL = GAME_4_NUMBER_STRING.match(REGEX_ONE_DIGIT)? true:false;
+
+  console.log(GAME_1_BOOL);
+
+  const NUMBER_STRING_1 = GAME_1_BOOL ? GAME_1_NUMBER_STRING:false;
+  const NUMBER_STRING_2 = GAME_2_BOOL ? GAME_2_NUMBER_STRING:false;
+  const NUMBER_STRING_3 = GAME_3_BOOL ? GAME_3_NUMBER_STRING:false;
+  const NUMBER_STRING_4 = GAME_4_BOOL ? GAME_4_NUMBER_STRING:false;
+
+  const STRING_NUMBERS = (NUMBER_STRING_1.length === 1 && NUMBER_STRING_1 && NUMBER_STRING_2 && NUMBER_STRING_2.length === 1 && NUMBER_STRING_3 && NUMBER_STRING_3.length === 1 && NUMBER_STRING_4 && NUMBER_STRING_4.length === 1)?NUMBER_STRING_1+NUMBER_STRING_2+NUMBER_STRING_3+NUMBER_STRING_4:alert("NaN and Number should have only one digit!");
+  //return STRING_NUMBERS;
+  GAME_ID.innerHTML = solve24(STRING_NUMBERS);
+
+  console.log(solve24(STRING_NUMBERS));
+  console.log(STRING_NUMBERS);
+}
+
+BUTTON_GET_TWENTY_FOUR.addEventListener( "click", (e) => {
+  getNumberTerm();
+});
+
 console.log(solve24('6789'));
-console.log(STRING_NUMBERS);
+                                        
